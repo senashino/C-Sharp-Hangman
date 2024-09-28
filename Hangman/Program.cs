c45868e8e6c1a42b1a5f5b80e1f6520b265137d0
TheCodersCat <54601614+TheCodersCat@users.noreply.github.com>
2024-09-28 14:45:42 +0100

    Initial commit

---
+++ Hangman/Program.cs
@@ -0,0 +1,45 @@
+﻿string word = "hello";
+
+int maxLives = 7;
+int currentLives = maxLives;
+
+bool win = false;
+
+List<char> guessedLetters = new List<char>();
+
+while(currentLives > 0 && !win)
+{
+    foreach(char c in word)
+    {
+        if (guessedLetters.Contains(c))
+            Console.Write(c);
+        else
+            Console.Write("_");
+    }
+    Console.WriteLine("\nPlease guess a letter!");
+    Console.WriteLine(currentLives + "/" + maxLives + " lives remaining.");
+
+    char guess = Convert.ToChar(Console.ReadLine());
+
+    if(word.Contains(guess) && !guessedLetters.Contains(guess))
+        Console.WriteLine("Correct!");
+    else
+    {
+        Console.WriteLine("Incorrect!");
+        currentLives--;
+    }
+    guessedLetters.Add(guess);
+
+    bool wordComplete = true;
+
+    foreach(char c in word)
+        if(!guessedLetters.Contains(c))
+            wordComplete = false;
+
+    win = wordComplete;
+}
+
+if (win)
+    Console.WriteLine("Congratulations, you win!");
+else
+    Console.WriteLine("You lose...");
